81ae0d7ad59915a72fa7384432b37d1fbfee3c9d
Deividas Šantaras <59109725+Deividas951@users.noreply.github.com>
2021-10-05 22:54:22 +0300

    Need to locate errors

--- IS_Lab2_Deividas_Santaras.m
+++ IS_Lab2_Deividas_Santaras.m
@@ -20,6 +20,54 @@ b1_2 = rand(1)*0,2;
 
 n = 0.1;
 % Network's response
+for indx = 1:length(x)
+    v1_1 = x(indx)* w11_1 + b1_1;
+    v2_1 = x(indx)* w21_1 + b2_1;
+    v3_1 = x(indx)* w31_1 + b3_1;
+    v4_1 = x(indx)* w41_1 + b4_1;
+    % Activation functions
+    y1_1 = tanh(v1_1);
+    y2_1 = tanh(v2_1);
+    y3_1 = tanh(v3_1);
+    y4_1 = tanh(v4_1);
+    % Second layer
+    v1_2 = y1_1.*w11_2+b1_2;
+    v2_2 = y2_1.*w12_2+b1_2;
+    v2_3 = y3_1.*w13_2+b1_2;
+    v2_4 = y4_1.*w14_2+b1_2;
+    % Output Activation function
+    y1_2 = 3*x+9;
+    % Error calculation
+    y = y1_2;
+    e = d(indx) - y;
+    % Error output layer gradient calculation
+    delta1_2 = (3*y1_2).*e;
+    % Error hidden layer gradient calculation
+    delta1_1 = y1_1.*(1-y1_1).*delta1_2*w11_2;
+    delta2_1 = y2_1.*(1-y2_1).*delta1_2*w12_2;
+    delta3_1 = y3_1.*(1-y3_1).*delta1_2*w13_2;
+    delta4_1 = y4_1.*(1-y4_1).*delta1_2*w14_2;
+   % Update output layer weights
+    w11_2 = w11_2 + n*delta1_2*y1_1;
+    w12_2 = w12_2 + n*delta1_2*y2_1;
+    w13_2 = w13_2 + n*delta1_2*y3_1;
+    w14_2 = w14_2 + n*delta1_2*y4_1;
+    b1_2 = b1_2 + n*delta1_2;
+   % Update hidden layer weights
+   w11_1 = w11_1 + n*delta1_1*x(indx);
+   w21_1 = w21_1 + n*delta2_1*x(indx);
+   w31_1 = w31_1 + n*delta3_1*x(indx);
+   w41_1 = w41_1 + n*delta4_1*x(indx);
+   b1_1 = b1_1 + n*delta1_1;
+   b2_1 = b2_1 + n*delta2_1;
+   b3_1 = b3_1 + n*delta3_1;
+   b4_1 = b4_1 + n*delta4_1;
+ 
+   
+   
+end
+
+Y = zeroes(1,length(x));
 for indx = 1:Length(x)
     v1_1 = x(indx)* w11_1 + b1_1;
     v2_1 = x(indx)* w21_1 + b2_1;
@@ -39,30 +87,5 @@ for indx = 1:Length(x)
     y1_2 = 3*x+9;
     % Error calculation
     y = y1_2;
-    e = d(indx) - y;
-    % Error output layer gradient calculation
-    delta1_2 = (3*y_1_2)*e;
-    % Error hidden layer gradient calculation
-    delta1_1 = y1_1*(1-y1_1)*delta1_2*w11_2;
-    delta2_1 = y2_1*(1-y2_1)*delta1_2*w12_2;
-    delta3_1 = y3_1*(1-y3_1)*delta1_2*w13_2;
-    delta4_1 = y4_1*(1-y4_1)*delta1_2*w14_2;
-   % Update output layer weights
-    w11_2 = w11_2 + n*delta1_2*y1_1;
-    w12_2 = w12_2 + n*delta1_2*y2_1;
-    w13_2 = w13_2 + n*delta1_2*y3_1;
-    w14_2 = w14_2 + n*delta1_2*y4_1;
-    b1_2 = b1_2 + n*delta1_2;
-   % Update hidden layer weights
-   w11_1 = w11_1 + n*delta1_1*x(indx);
-   w12_1 = w12_1 + n*delta2_1*x(indx);
-   w13_1 = w13_1 + n*delta3_1*x(indx);
-   w14_1 = w14_1 + n*delta4_1*x(indx);
-   b1_1 = b1_1 + n*delta1_1;
-   b2_1 = b2_1 + n*delta2_1;
-   b3_1 = b3_1 + n*delta3_1;
-   b4_1 = b4_1 + n*delta4_1;
- 
-   
-   
+    Y(indx) = y;
 end
