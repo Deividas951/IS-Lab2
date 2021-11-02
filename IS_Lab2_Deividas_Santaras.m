788e10f05e4c22fc72573dbb1833d48ab841c5b3
Deividas Šantaras <59109725+Deividas951@users.noreply.github.com>
2021-11-02 18:27:28 +0200

    Fixed

--- IS_Lab2_Deividas_Santaras.m
+++ IS_Lab2_Deividas_Santaras.m
@@ -1,25 +1,29 @@
 % Data preperation
-x = [0.1:1/52:1];
-d =  (2 + 0.3*cos(2*pi*x/0.2)) + 0.5*sin(4*pi*x + 2)./3;
+x = [0.1:1/152:1];
+d =  ((2 + 0.3*cos(2*pi*x/0.2)) + 0.5*sin(4*pi*x + 2)./9)+2;
 figure(1), plot(x, d, 'r*'), grid on;
 % First layer
 w11_1 = randn(1);
 w21_1 = randn(1);
 w31_1 = randn(1);
 w41_1 = randn(1);
+w51_1 = randn(1);
 b1_1 = randn(1);
 b2_1 = randn(1);
 b3_1 = randn(1);
 b4_1 = randn(1);
+b5_1 = randn(1);
 % Second layer
 w11_2 = randn(1);
 w12_2 = randn(1);
 w13_2 = randn(1);
 w14_2 = randn(1);
+w15_2 = randn(1);
 b1_2 = randn(1);
 cycle = 0;
-n = 0.2;
+n = 0.002;
 %% Network's response
+for ind = 1:1000000;
 for indx = 1:length(x)
     v1_1 = x(indx)* w11_1 + b1_1;
     v2_1 = x(indx)* w21_1 + b2_1;
@@ -32,21 +36,18 @@ for indx = 1:length(x)
     y4_1 = tanh(v4_1);
     % Second layer
     v1_2 = y1_1*w11_2+y2_1*w12_2+y3_1*w13_2+y4_1*w14_2+b1_2;
-%     v2_2 = y2_1.*w12_2+b1_2;
-%     v2_3 = y3_1.*w13_2+b1_2;
-%     v2_4 = y4_1.*w14_2+b1_2;
     % Output Activation function
-    y1_2 = 3*v1_2+2; 
+    y1_2 = 3*v1_2+2; %3*x+2
     % Error calculation
     y = y1_2;
     e = d(indx) - y;
     % Error output layer gradient calculation
     delta1_2 = 3*e;
     % Error hidden layer gradient calculation
-    delta1_1 = (1-(tanh(y1_1))^2)*delta1_2.*w11_2;
-    delta2_1 = (1-(tanh(y2_1))^2)*w12_2;
-    delta3_1 = (1-(tanh(y3_1))^2)*w13_2;
-    delta4_1 = (1-(tanh(y4_1))^2)*w14_2;
+    delta1_1 = (1-(tanh(v1_1))^2)*delta1_2*w11_2;
+    delta2_1 = (1-(tanh(v2_1))^2)*delta1_2*w12_2;
+    delta3_1 = (1-(tanh(v3_1))^2)*delta1_2*w13_2;
+    delta4_1 = (1-(tanh(v4_1))^2)*delta1_2*w14_2;
    % Update output layer weights
     w11_2 = w11_2 + n.*delta1_2.*y1_1;
     w12_2 = w12_2 + n.*delta1_2.*y2_1;
@@ -66,6 +67,7 @@ for indx = 1:length(x)
    
    
 end
+end
 cycle2 = 0;
 %% Y = zeros(1,length(x));
 for indx = 1:length(x)
@@ -81,12 +83,13 @@ for indx = 1:length(x)
     % Second layer
     v1_2 = y1_1*w11_2+y2_1*w12_2+y3_1.*w13_2+y4_1.*w14_2+b1_2;
     % Output Activation function
-    y1_2 = 3*v1_2+9;
+    y1_2 = 3*v1_2+2; %3*x+5
     % Error calculation
     yfin = y1_2;
     Y(indx) = yfin;
     cycle2 = cycle2+1;
 end
 figure
+ylim([0,5])
 plot(x, d, 'r*',x, Y);
 
